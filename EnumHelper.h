33346cc5971d4b7ea407e99bad9004dc2a43d280
hchlqlz <614563449@qq.com>
2022-03-10 22:09:44 +0800

    代码优化。

--- EnumHelper.h
+++ EnumHelper.h
@@ -1,24 +1,19 @@
 #pragma once
 
-#include <stdio.h>
-#include <iostream>
-#include <cmath>
 #include <map>
 #include <algorithm>
 #include <string>
-#include <sstream>
 #include <vector>
 #include <functional>
-using namespace std;
 
-vector<string> split(const string& str, const string& sep);
+std::vector<std::string> split(const std::string& str, const std::string& sep);
 
 template<typename T>
-string EnumHelper(T key, const std::function<char(char)> processor = nullptr, const char* pszName = NULL)
+std::string EnumHelper(T key, const std::function<char(char)> processor = nullptr, const char* pszName = NULL)
 {
     static_assert(std::is_enum_v<T>, __FUNCTION__ "'s key need a enum");
 
-    static map<T, string> s_mapName;
+    static std::map<T, std::string> s_mapName;
 
     if (nullptr != pszName)
     {
@@ -41,14 +36,21 @@ string EnumHelper(T key, const std::function<char(char)> processor = nullptr, co
 }
 
 template <class T>
-size_t analystEnum(T enumClass, const char* pszNames)
+size_t ParseEnum(T enumClass, const char* pszNames)
 {
     static_assert(std::is_enum_v<T>, __FUNCTION__ "'s enumClass need a enum");
 
+    static size_t s_sizeOfEnum = 0;
+
+    // 避免重复调用的开销
+    if (s_sizeOfEnum != 0)
+    {
+        return s_sizeOfEnum;
+    }
+
     if (nullptr != pszNames)
     {
-        const vector<string>& vecName = split(pszNames, ",");
-
+        const std::vector<std::string>& vecName = split(pszNames, ",");
         for (int i = 0; i < vecName.size(); ++i)
         {
             if (vecName.at(i).size() > 0)
@@ -57,13 +59,12 @@ size_t analystEnum(T enumClass, const char* pszNames)
             }
         }
 
-        return vecName.size();
+        s_sizeOfEnum = vecName.size();
+        return s_sizeOfEnum;
     }
 
     return 0;
 }
-
-#define ENUM_DEFINE(type, ...) enum class type { placeholder, __VA_ARGS__ }; const size_t g_uEnumSizeOf##type = analystEnum(type::placeholder, #__VA_ARGS__);
 
 /*
 ENUM_DEFINE 用法示例：
@@ -78,3 +79,4 @@ EnumHelper(Color::Red, std::toupper) -> "RED"
 注意点：
 1、枚举值只能系统自增，不能写 Red = 1，代码未处理该种情况
 */
+#define ENUM_DEFINE(type, ...) enum class type { placeholder, __VA_ARGS__ }; const size_t g_uEnumSizeOf##type = ParseEnum(type::placeholder, #__VA_ARGS__);
